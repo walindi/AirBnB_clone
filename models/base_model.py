0e3d3db56528b80a9569c1319907f090f775d4db
walindi <walindibarack@yahoo.com>
2023-03-08 08:29:09 +0300

    Added methods

--- models/base_model.py
+++ models/base_model.py
@@ -20,8 +20,26 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+    def save(self):
+        """Public instance method
+        Updates the public intance attribute updated_at with
+        the current datetime"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+
+        dictionary = self.__dict__.copy()
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary["__class__"] = self.__class__.__name__
+        return dictionary
+
     def __str__(self):
         """Return string representation of the BaseModel instance"""
 
         className = self.__class__.__name__
-        return f"[{className}] (self.id) self.__dict__"
+        return f"[{className}] ({self.id}) {self.__dict__}"
