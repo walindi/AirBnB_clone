4078b36b6a952ba31733535d86e1583e4463207e
walindi <walindibarack@yahoo.com>
2023-03-08 08:44:05 +0300

    Unittests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Unittests for models/base_model.py"""
+
+import unittest
+import models
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Test instantiation of the BaseModel class"""
+
+
+class TestBaseModel_save(unittest.TestCase):
+    """Test save method of BaseModel class"""
+
+
+class TestBaseModel_to_dict(unittest.TestCase):
+    """Test to_dict method"""
+
+
+if __name__ == "__main__":
+    unittest.main()
