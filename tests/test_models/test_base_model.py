1662943df17f1b655e30c61708063b9625181fa4
walindi <walindibarack@yahoo.com>
2023-03-10 09:14:42 +0300

    More tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,19 +14,23 @@ class TestBaseModel_instantiation(unittest.TestCase):
     def test_no_args_instantiates(self):
         self.assertEqual(BaseModel, type(BaseModel()))
 
+    def test_if_BaseModel_instance_has_id(self):
+        b = BaseModel()
+        self.assertTrue(hasattr(b, "id"))
+
     def test_id_is_public_str(self):
         self.assertEqual(str, type(BaseModel().id))
+
+    def test_two_models_unique_ids(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
 
     def test_created_at_is_public_datetime(self):
         self.assertEqual(datetime, type(BaseModel().created_at))
 
     def test_updated_at_is_public_datetime(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
-
-    def test_two_models_unique_ids(self):
-        bm1 = BaseModel()
-        bm2 = BaseModel()
-        self.assertNotEqual(bm1.id, bm2.id)
 
     def test_str_representation(self):
         dt = datetime.today()
@@ -44,9 +48,43 @@ class TestBaseModel_instantiation(unittest.TestCase):
 class TestBaseModel_save(unittest.TestCase):
     """Test save method of BaseModel class"""
 
+    def test_that_save_func_updates_update_at_attr(self):
+        b = BaseModel()
+        b.save()
+        self.assertNotEqual(b.created_at, b.updated_at)
+        self.assertGreater(b.updated_at.microsecond,
+                           b.created_at.microsecond)
+
+    def test_save_updates_file(self):
+        bm = BaseModel()
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
+
 
 class TestBaseModel_to_dict(unittest.TestCase):
     """Test to_dict method"""
+
+    def test_if_to_dict_returns_dict(self):
+        b = BaseModel()
+        self.assertTrue(type(b.to_dict()) is dict)
+
+    def test_to_dict_contains_added_attributes(self):
+        b = BaseModel()
+        attrs = ["id", "created_at", "updated_at", "__class__"]
+        b.name = "Walindi"
+        b.email = "walindi@123.com"
+        attrs.extend(["name", "email"])
+        for attr in attrs:
+            self.assertIn(attr, b.to_dict())
+
+    def test_to_dict_contains_correct_keys(self):
+        bm = BaseModel()
+        self.assertIn("id", bm.to_dict())
+        self.assertIn("created_at", bm.to_dict())
+        self.assertIn("updated_at", bm.to_dict())
+        self.assertIn("__class__", bm.to_dict())
 
 
 if __name__ == "__main__":
