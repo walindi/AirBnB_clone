1b478362d7afd5a05ca1e2969c678220a96d49f0
walindi <walindibarack@yahoo.com>
2023-03-21 22:22:01 +0300

    Update

--- console.py
+++ console.py
@@ -2,12 +2,36 @@
 """Entry point of the command interpreter"""
 
 import cmd
+import re
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def do_quit(self, line):
+        """Exits the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Exits the program"""
+        print("")
+        return True
+
+    def do_create(self, line):
+        """Creates a new instance of 'BaseModel',
+        saves (it to the JSON file) and prints the id"""
+
+        rex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(rex, line)
+        if not match:
+            print("** class name missing **")
+            return
+
+        class_name = match.group(1)
+        if c_name not in storage.dictionary():
+            print("** class doesn't exist **")
+            return
 
 
 if __name__ == '__main__':
